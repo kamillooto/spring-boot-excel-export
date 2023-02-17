49fe4922d5d847d5a0871c5b331153a9e9d09bde
Kamil Otocki <kamillooto@gmail.com>
2023-02-17 07:24:04 +0100

    First commit

---
+++ src/test/java/com/kamillooto/springbootexcelexport/SpringbootExcelExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamillooto.springbootexcelexport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExcelExportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
