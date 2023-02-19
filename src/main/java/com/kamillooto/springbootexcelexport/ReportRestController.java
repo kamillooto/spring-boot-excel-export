59cb9b805a329a4b468ad30fce1d6726652018c0
Kamil Otocki <kamillooto@gmail.com>
2023-02-19 18:09:07 +0100

    Add ReportRestController

---
+++ src/main/java/com/kamillooto/springbootexcelexport/ReportRestController.java
@@ -0,0 +1,29 @@
+package com.kamillooto.springbootexcelexport;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class ReportRestController {
+
+    @Autowired
+    private ReportService reportService;
+
+    @GetMapping("/excel")
+    public void generateExcelReport(HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/octet-stream");
+
+        String headerKey = "Content Disposition";
+        String headerValue = "attachment;filename-courses.xls";
+
+        response.setHeader(headerKey, headerValue);
+
+        reportService.generateExcel(response);
+    }
+
+}
