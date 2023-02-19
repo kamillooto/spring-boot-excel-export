c6f2b8929ef3a50cb64c3c388be1e9a7c86bf074
Kamil Otocki <kamillooto@gmail.com>
2023-02-19 17:40:09 +0100

    Add Course entity, fix SpringbootExcelExportApplication.java class and change application.properties

---
+++ src/main/java/com/kamillooto/springbootexcelexport/Course.java
@@ -0,0 +1,17 @@
+package com.kamillooto.springbootexcelexport;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "COURSE_DTLS")
+public class Course {
+
+    @Id
+    private Integer cid;
+    private String cname;
+    private Double price;
+}
